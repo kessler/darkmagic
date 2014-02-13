a0354425b2aa732bbad39536181e9424e0e7c199
Yaniv Kessler <yanivk@gmail.com>
2014-02-13 04:51:46 +0200

    type check in inject

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 module.exports.Container = require('./lib/Container.js');
 module.exports.inject = function(file) {
 	var container = new Container();
-	var main = require(file);
+
+	var main;
+
+	if (typeof file === 'string')
+		main = require(file);
+	else if (typeof file !== 'function')
+		throw new Error('invalid parameter, must provide a filename or a function');
+
 	container.inject(main);
 };
